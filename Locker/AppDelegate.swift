060cec63e29298aee6743610792034faab980606
Bhuridech Sudsee <dekcomgang@hotmail.com>
2017-04-22 16:07:49 +0700

    Initial Commit

---
+++ Locker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Locker
+//
+//  Created by Bhuridech Sudsee on 4/22/2560 BE.
+//  Copyright © 2560 Bhuridech Sudsee. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Locker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Locker
+//
+//  Created by Bhuridech Sudsee on 4/22/2560 BE.
+//  Copyright © 2560 Bhuridech Sudsee. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
